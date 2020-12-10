ad516a5d3f2f3b2b2f9bd28479a84d2cc27a197a
Brendan Loomis <brendanloomis@gmail.com>
2020-12-09 16:54:44 -0800

    Removed commented out code

--- src/App.js
+++ src/App.js
@@ -22,30 +22,5 @@ function App(props) {
     </main>
   );
 }
-/*class App extends Component {
-  static defaultProps = {
-    store: {
-      lists: [],
-      allCards: {},
-    }
-  };
-
-  render() {
-    const { store } = this.props;
-    return (
-      <main className='App'>
-        <header className='App-header'>
-          <h1>Trelloyes!</h1>
-        </header>
-        <div className='App-list'>
-          {store.lists.map(list => (
-            <List 
-              key={list.id}
-          ))}
-        </div>
-      </main>
-    )
-  }
-}*/
 
 export default App;
